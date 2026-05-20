f2c36fbfaa2ecc94032fafbcf50e71668913f82a
Laasya Chinni <laasyachinni2007@gmail.com>
2026-05-20 16:14:41 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,34 @@
+print("laasya","chinni")
+print("AIML")
+print("Branch")      
+
+name = "laasya"
+roll = 24
+print(name,roll)
+print(type(name))
+print(type(roll))
+
+#Expression execution
+A=3
+B=2
+Txt="@"
+print(A*Txt*B)
+
+X="2"
+Txt="@"
+Y=3
+print((X+Txt)*Y)
+ 
+a=15
+b=-2
+print(a/b) #whenever deno is nag rem also neg
+
+a=34
+b=3
+print(a//b) #integer redivision gives the floor value
+
+#INPUT STATEMENT
+name=input("name:")
+age=int(input("age:"))
+price=float(input("price:"))
+
