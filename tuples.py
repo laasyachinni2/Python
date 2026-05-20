37b3753a6b8385f2688b784e0af28aad7394aac7
Laasya Chinni <laasyachinni2007@gmail.com>
2026-05-20 16:13:22 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,16 @@
+tup=(1,2,3,4,5)
+print(type(tup))
+#-->if a singe ele in the tuple compulsory to put comma
+
+#INDEXING
+print(tup[2])
+#--> ASSIGNING IS NOT POSSIBLE
+
+#SLICING
+print(tup[1:4])
+print(tup[:5])
+print(tup[1:])
+
+#TUPLE METHODS
+print(tup.index(2))
+print(tup.count(4))
